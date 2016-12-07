8305103659ccad7b43e6edd3285eb34ae6e0fcdc
icss123456 <chenwj@chinasofti.com>
2016-12-07 20:47:42 +0800

    初始化

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+// 轮播插件
+var mySwiper = new Swiper('.swiper-container',{
+    loop: true,
+	autoplay: 3000,
+	pagination: '.swiper-pagination',
+	nextButton: '.swiper-button-next',
+	prevButton: '.swiper-button-prev',			
+	paginationClickable: true,
+	autoplayDisableOnInteraction: false,
+
+});
